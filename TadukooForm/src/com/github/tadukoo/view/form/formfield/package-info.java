1c57c1d57f852448788063b0f0e75585cbfa6f59
Tadukoo <realtadukoo@gmail.com>
2023-03-25 21:55:38 -0400

    Adding some missing package-info's

---
+++ TadukooForm/src/com/github/tadukoo/view/form/formfield/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Contains {@link com.github.tadukoo.view.form.field.FormField FormFields} for Forms
+ */
+package com.github.tadukoo.view.form.formfield;
---
+++ TadukooFormComponents/src/com/github/tadukoo/view/form/components/field/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Contains {@link com.github.tadukoo.view.form.field.FormField FormFields} for components made of Forms
+ */
+package com.github.tadukoo.view.form.components.field;
